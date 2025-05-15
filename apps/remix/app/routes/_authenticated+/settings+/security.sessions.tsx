da6424c90c3b47d12c103543fd97d455210e7832
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2025-05-15 13:40:21 +0000

    chore: use dialog

--- apps/remix/app/routes/_authenticated+/settings+/security.sessions.tsx
+++ apps/remix/app/routes/_authenticated+/settings+/security.sessions.tsx
@@ -1,9 +1,7 @@
 import { msg } from '@lingui/core/macro';
 import { useLingui } from '@lingui/react';
 
-import { authClient } from '@documenso/auth/client';
-import { Button } from '@documenso/ui/primitives/button';
-
+import { SessionLogoutAllDialog } from '~/components/dialogs/session-logout-all-dialog';
 import { SettingsHeader } from '~/components/general/settings-header';
 import { SettingsSecuritySessionTable } from '~/components/tables/settings-security-session-table';
 import { appMetaTags } from '~/utils/meta';
@@ -22,14 +20,7 @@ export default function SettingsSecuritySessions() {
         subtitle={_(msg`View and manage all active sessions for your account.`)}
         hideDivider={true}
       >
-        <Button
-          variant="secondary"
-          onClick={async () =>
-            authClient.signOutAllSessions({ redirectPath: '/settings/security/sessions' })
-          }
-        >
-          Revoke all sessions
-        </Button>
+        <SessionLogoutAllDialog />
       </SettingsHeader>
 
       <div className="mt-4">
